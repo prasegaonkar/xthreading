648574dff7724d9bb63459be72c4661660ae5fb8
prasegaonkar <prashant.rasegaonkar@gmail.com>
2018-10-18 10:44:20 +0530

    threadpool with executorservice

---
+++ src/xthreading/BasicThreadPool.java
@@ -0,0 +1,39 @@
+package xthreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class BasicThreadPool {
+	public static void main(String[] args) {
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		for (int i = 1; i <= 5; i++) {
+			executor.submit(new Processor(i));
+		}
+		executor.shutdown();
+		System.out.println("All tasks submitted.");
+		try {
+			executor.awaitTermination(1, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+		}
+		System.out.println("All tasks completed.");
+	}
+
+	private static class Processor implements Runnable {
+		private int id;
+
+		public Processor(int id) {
+			this.id = id;
+		}
+
+		public void run() {
+			System.out.println("Starting " + id);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+			System.out.println("Completed " + id);
+		}
+
+	}
+}
